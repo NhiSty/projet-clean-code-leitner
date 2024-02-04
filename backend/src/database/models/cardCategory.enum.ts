63c75fc85fecddec72b78c5d922c73e866f9d34a
Serkan Deveci <serkand27@hotmail.com>
2024-02-04 16:24:59 +0100

    fix bugs

--- backend/src/database/models/cardCategory.enum.ts
+++ backend/src/database/models/cardCategory.enum.ts
@@ -2,12 +2,12 @@
  * CardCategory is an enum that represents the category of a card.
  */
 export enum CardCategory {
-    FIRST = "first",
-    SECOND = "second",
-    THIRD = "third",
-    FOURTH = "fourth",
-    FIFTH = "fifth",
-    SIXTH = "sixth",
-    SEVENTH = "seventh",
-    DONE = "done",
+  FIRST = "first",
+  SECOND = "second",
+  THIRD = "third",
+  FOURTH = "fourth",
+  FIFTH = "fifth",
+  SIXTH = "sixth",
+  SEVENTH = "seventh",
+  DONE = "done",
 }
