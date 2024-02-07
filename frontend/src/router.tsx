d0211e9605887d862c5ea87f61dde83272855704
Thomas J <thomas.jallu@gmail.com>
2024-02-07 23:43:47 +0100

    Add pages in router

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,10 +1,50 @@
 import { createBrowserRouter } from "react-router-dom";
 import { App } from "./App";
+import { ErrorPage } from "./pages/errorPage";
+import { HomePage } from "./pages/homePage";
+import { CardsPage } from "./pages/cards/cardsPage";
+import { NewCardPage } from "./pages/cards/newCardPage";
+import { LearningPage } from "./pages/learning/learningPage";
+import { LearningLayoutPage } from "./pages/learning/learningLayoutPage";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <App/>,
+        errorElement: <ErrorPage/>,
+        children: [
+            {
+                path: '/',
+                element: <HomePage/>,
+            },
+            {
+                path: '/cards',
+                children: [
+                    {
+                        path: '/cards',
+                        element: <CardsPage/>,
+                    },
+                    {
+                        path: '/cards/new',
+                        element: <NewCardPage/>,
+                    },
+                ]
+            },
+            {
+                path: '/learning',
+                element: <LearningLayoutPage/>,
+                children: [
+                    {
+                        path: '/learning',
+                        element: <LearningPage/>,
+                    },
+                    {
+                        path: '/learning/:date',
+                        element: <LearningPage/>,
+                    }
+                ]
+            }
+        ]
     }
 ])
 
